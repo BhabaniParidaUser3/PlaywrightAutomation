04d214f9147a76d5f80df7fb2f1d7b88dda11d26
BhabaniParidaUser3 <bhabaniparida1995@gmail.com>
2026-08-21 02:23:54 +0530

    test case developed for Verify tab key and Verify Escape key closes dropdown

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -189,17 +189,35 @@ import { test, expect } from '@playwright/test';
 
 
 
-test('Verify Login using enter key',async({page})=>
+// test('Verify tab key',async({page})=>
+// {
+// await page.goto('https://www.saucedemo.com/');
+// const username= page.getByPlaceholder('Username');
+// await username.fill('standard_user');
+// await username.press('Tab');
+//  const password= page.getByPlaceholder('Password');
+// await password.fill('secret_sauce');
+// await password.press('Enter');
+// await expect(page.getByText('Swag Labs')).toBeVisible();
+// await page.pause();
+// }
+// );
+
+
+
+
+test('Verify Escape key closes dropdown',async({page})=>
 {
-await page.goto('https://www.saucedemo.com/');
-await page.getByPlaceholder('Username').fill('standard_user');
-const password= page.getByPlaceholder('Password');
-await password.fill('secret_sauce');
-await password.press('Enter');
-await expect(page.getByText('Swag Labs')).toBeVisible();
-await page.pause();
-}
-);
+  await page.goto('https://opensource-demo.orangehrmlive.com/');
+  await page.getByPlaceholder('Username').fill('Admin');
+ await page.getByPlaceholder('Password').fill('admin123');
+await page.getByRole('button' ,{name:'Login'}).click();
+  await page.getByRole('link',{name:'Admin'}).click();
+  const dropdown= page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow');
+  await dropdown.first().click();
+  await page.keyboard.press('Escape');
+  await expect(page.getByRole('listbox')).not.toBeVisible();
+ await page.pause();
 
-
-
+  }
+)
